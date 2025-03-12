81ac4bc3aaa56271e9112f7ab3864e5a2f16001f
Dm-Rb <dm.rb.418http@gmail.com>
2025-03-12 15:26:17 +0300

    1

---
+++ image_processing.py
@@ -0,0 +1,84 @@
+from PIL import Image
+from io import BytesIO
+import imghdr
+
+
+def max_image(image_obj, w, h, extension):
+    if extension == 'jpg':
+        extension = 'jpeg'
+    # Получаем текущие размеры изображения
+    original_width, original_height = image_obj.size
+
+    # Вычисляем соотношение сторон
+    ratio = min(w / original_width, h / original_height)
+
+    # Вычисляем новые размеры с сохранением пропорций
+    new_width = int(original_width * ratio)
+    new_height = int(original_height * ratio)
+
+    # Изменяем размер изображения
+    resized_image = image_obj.resize((new_width, new_height), Image.Resampling.LANCZOS)
+
+    # Создаем объект BytesIO для хранения изображения
+    output = BytesIO()
+
+    # Сохраняем изображение
+    # format=extension.upper() заменяем фактический тип файла на прописанный (есть вероятность несовпадения)
+    resized_image.save(output, format=extension.upper())
+
+    # Перемещаем указатель в начало файла
+    output.seek(0)
+
+    return output
+
+
+
+def crop_image(image_obj, crop_width, crop_height, extension):
+    if extension == 'jpg':
+        extension = 'jpeg'
+    original_width, original_height = image_obj.size
+
+    # Вычисляем соотношения сторон
+    original_aspect = original_width / original_height
+    crop_aspect = crop_width / crop_height
+
+    if original_aspect >= crop_aspect:
+        # Обрезаем по ширине
+        use_width = int(original_height * crop_aspect)
+        x = (original_width - use_width) / 2
+        y = 0
+        use_height = original_height
+    else:
+        # Обрезаем по высоте
+        use_height = int(original_width / crop_aspect)
+        y = (original_height - use_height) / 2
+        x = 0
+        use_width = original_width
+
+    # Обрезаем изображение
+    cropped_image = image_obj.crop((x, y, x + use_width, y + use_height))
+
+    # Масштабируем до целевого размера
+    resized_image = cropped_image.resize((crop_width, crop_height), Image.Resampling.LANCZOS)
+
+
+    # Создаем объект BytesIO для хранения изображения
+    output = BytesIO()
+
+    # Сохраняем изображение
+    # format=extension.upper() заменяем фактический тип файла на прописанный (есть вероятность несовпадения)
+    cropped_image.save(output, format=extension.upper())
+
+    # Перемещаем указатель в начало файла
+    output.seek(0)
+
+    return output
+
+
+
+def get_actual_image_type(image_data: bytes) -> str:
+    """
+    Определяет фактический тип изображения на основе его сигнатуры.
+    """
+    actual_type = imghdr.what(None, image_data)
+    return actual_type if actual_type else "unknown"
---
+++ server.py
@@ -0,0 +1,86 @@
+from fastapi import FastAPI, HTTPException, Query
+from fastapi.responses import Response
+import httpx
+from io import BytesIO
+from PIL import Image
+from urllib.parse import urlparse
+import os
+from image_processing import max_image, crop_image, get_actual_image_type
+
+app = FastAPI()
+IMAGE_STORAGE_HOST = "http://minio-s3.first.belpak.lan:9000"
+
+
+def get_file_extension(url):
+    """
+    Функция извлекает из урла расширение файла если оно есть
+    """
+    # Разбираем URL
+    parsed_url = urlparse(url)
+
+    # Извлекаем путь к файлу
+    path = parsed_url.path
+
+    # Извлекаем расширение файла
+    extension = os.path.splitext(path)[1]
+
+    # Убираем точку из расширения, если она есть
+    if extension:
+        extension = extension.lstrip('.')
+
+    return extension
+
+
+
+
+# Словарь доступных обработчиков
+PROCESSORS = {
+    "max": max_image,
+    "crop": crop_image,
+}
+
+
+@app.get("/{image_path:path}")
+async def process_image(
+    image_path: str,
+    mode: str = Query(None),
+    width: int = Query(None),
+    height: int = Query(None)
+):
+    # Тут фактический url до изображения на файловом хранилище
+    image_url = f"{IMAGE_STORAGE_HOST}/{image_path}"
+    # Расширение файла (если есть)
+    extension = get_file_extension(image_path)
+    if extension:
+        extension = extension.lower()
+    else:
+        extension = 'unknown'
+
+    async with httpx.AsyncClient() as client:
+
+        # Запрос на файловое хранилище
+        response = await client.get(image_url)
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=response.status_code, detail="Image not found")
+
+        # Если нет параметров вернуть оригинальное изображение без обработки
+        if (not mode) or (not width) or (not height):
+            return Response(content=response.content, media_type=f"image/{get_actual_image_type(response.content)}")
+
+        # Если mode указан, но нет width/height – возбудить исключение
+        if mode and (width is None or height is None):
+            raise HTTPException(status_code=500, detail="Width and height are required with mode")
+
+        if mode not in PROCESSORS:
+            raise HTTPException(status_code=500, detail="Unsupported mode")
+
+        # Обрабатываем изображение
+        try:
+            image = Image.open(BytesIO(response.content))
+            processed_image = PROCESSORS[mode](image, width, height, extension)
+            return Response(content=processed_image.getvalue(), media_type=f"image/{extension}")
+        except Exception as _ex:
+            raise HTTPException(status_code=500, detail=f"An error occurred while processing the image file: {_ex}")
+
+# uvicorn server:app --host 127.0.0.1 --port 9000 --reload
